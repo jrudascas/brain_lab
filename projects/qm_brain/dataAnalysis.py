ca73eda93b0d1303119c1106cc601ede3f5dc5f1
NPopiel <nicholas.popiel@bell.net>
2019-06-11 12:24:10 -0400

    Commit1

--- projects/qm_brain/dataAnalysis.py
+++ projects/qm_brain/dataAnalysis.py
@@ -1,109 +1,103 @@
-import scipy.io
-from scipy.signal import hilbert
+from .utils import *
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
-from matplotlib import animation
-from .utils import *
+filepathMat = '/home/user/Desktop/QMBrain/EEG1.mat'
+filepathChanLoc = '/home/user/Desktop/QMBrain/chanLocXY.csv'
+filepathData = '/home/user/Desktop/QMBrain/data.csv'
+filepathTimes = '/home/user/Desktop/QMBrain/times.csv'
 
-filepathMat = '/home/user/Desktop/qm_brain/EEG1.mat'
-filepathChanLoc = '/home/user/Desktop/qm_brain/chanLocXY.csv'
-filepathData = '/home/user/Desktop/qm_brain/data.csv'
-filepathTimes = '/home/user/Desktop/qm_brain/times.csv'
-
-# dataSet = load_matrix(filepathMat)['EEG1']
 
 data = load_matrix(filepathData)
 times = load_matrix(filepathTimes)
 
 chanLocs = load_matrix(filepathChanLoc)
 
-x = chanLocs[:, 0]
-y = chanLocs[:, 1]
+x, y, wavefunction_position, phase, normAmp, probability = process_eeg_csv(data,chanLocs)
 
-hilbertTransData = hilbert(data)
+xAvg = probability@x
+yAvg = probability@y
 
-amplitude = np.abs(hilbertTransData)
+xSqrAvg = probability@(x*x)
+ySqrAvg = probability@(y*y)
 
-phase = np.unwrap(np.angle(hilbertTransData))
+dx = np.sqrt(xSqrAvg-(xAvg*xAvg))
+dy = np.sqrt(ySqrAvg-(yAvg*yAvg))
 
-ampMag = np.linalg.norm(amplitude.T)
+#probability_conservation_plot(len(x),probability)
 
-normAmp = (np.asarray(amplitude.T) / np.asarray(ampMag))
+momentum_wavefunction = momentum_wavefunction(wavefunction_position)
+x = x
+momentum_prob = (np.conj(momentum_wavefunction).T)@momentum_wavefunction
 
-probability = normAmp * normAmp
 
-xAvg = probability @ x
-yAvg = probability @ y
+#animation_station(xAvg,yAvg,x,y,dx,dy)
 
-xSqrAvg = probability @ (x * x)
-ySqrAvg = probability @ (y * y)
+#Calculate momentum of brain state
 
-dx = np.sqrt(xSqrAvg - (xAvg * xAvg))
-dy = np.sqrt(ySqrAvg - (yAvg * yAvg))
-
-# Calculate momentum of brain state
-
-pxSum = np.zeros((len(times), len(x)), dtype=np.complex64)
+pxSum = np.zeros((len(times),len(x)),dtype=np.complex64)
 pxSqrSum = np.copy(pxSum)
 
-pySum = np.zeros((len(times), len(y)), dtype=np.complex64)
+pySum = np.zeros((len(times),len(y)),dtype=np.complex64)
 pySqrSum = np.copy(pySum)
 
 for i in range(len(x)):
     current_x = x[i]
     current_y = y[i]
 
-    # find closest nodes
+    #find closest nodes
 
     xDiff = current_x - x
     yDiff = current_y - y
 
-    xPairs = np.where(abs(xDiff) > abs(yDiff))
-    yPairs = np.where(abs(yDiff) > abs(xDiff))
+    xPairs = np.where(abs(xDiff)>abs(yDiff))
+    yPairs = np.where(abs(yDiff)>abs(xDiff))
 
-    distance = np.sqrt(np.square(xDiff) + np.square(yDiff))
+    distance = np.sqrt(np.square(xDiff)+np.square(yDiff))
 
     xPairDistance = distance[xPairs]
     yPairDistance = distance[yPairs]
 
-    firstX = sorted(xPairs)[-1][-1]
-    secondX = sorted(xPairs)[-1][-2]
+    firstX = sorted(xPairDistance)[0]
+    secondX = sorted(xPairDistance)[1]
+    firstXInd = np.where(distance==firstX)[0][0]
+    secondXInd = np.where(distance==secondX)[0][0]
 
-    firstY = sorted(yPairs)[-1][-1]
-    secondY = sorted(yPairs)[-1][-2]
+    firstY = sorted(yPairDistance)[0]
+    secondY = sorted(yPairDistance)[1]
+    firstYInd = np.where(distance == firstY)[0][0]
+    secondYInd = np.where(distance == secondY)[0][0]
 
-    diffX1 = x[firstX] - current_x
-    diffX2 = x[secondX] - current_x
+    diffX1 = x[firstXInd]-current_x
+    diffX2 = x[secondXInd] - current_x
 
-    diffY1 = x[firstY] - current_y
-    diffY2 = x[secondY] - current_y
+    diffY1 = x[firstYInd] - current_y
+    diffY2 = x[secondYInd] - current_y
 
-    psiConj = normAmp[:, i] * np.exp((-1j) * phase[i, :])
-    psi_n = normAmp[:, i] * np.exp((1j) * phase[i, :])
+    psiConj = normAmp[:,i]*np.exp((-1j)*phase[:,i])
+    psi_n = normAmp[:,i]*np.exp((1j)*phase[:,i])
 
-    psi_n1x = normAmp[:, firstX] * np.exp((1j) * (phase[firstX, :]))
-    psi_n2x = normAmp[:, secondX] * np.exp((1j) * (phase[secondX, :]))
+    psi_n1x = np.squeeze(normAmp[:,firstXInd]*np.exp((1j)*(phase[:,firstXInd])))
+    psi_n2x = np.squeeze(normAmp[:,secondXInd]*np.exp((1j)*(phase[:,secondXInd])))
 
-    psi_n1y = normAmp[:, firstY] * np.exp((1j) * (phase[firstY, :]))
-    psi_n2y = normAmp[:, secondY] * np.exp((1j) * (phase[secondY, :]))
+    psi_n1y = np.squeeze(normAmp[:,firstYInd]*np.exp((1j) * (phase[:,firstYInd])))
+    psi_n2y = np.squeeze(normAmp[:,secondYInd]*np.exp((1j) * (phase[:,secondYInd])))
 
-    pxSum[:, i] = 0.5 * (psiConj * ((psi_n1x - psi_n) / diffX1) + psiConj * ((psi_n2x - psi_n) / diffX2))
-    pxSqrSum[:, i] = 0.5 * (psiConj * ((psi_n - 2 * psi_n1x + psi_n2x) / np.square(diffX1)))
+    pxSum[:,i] = 0.5 * ((psiConj/diffX1)*(psi_n1x-psi_n)+(psiConj/diffX2)*(psi_n2x-psi_n))
+    pxSqrSum[:,i] = (psiConj*((psi_n-2*psi_n1x + psi_n2x)/np.square(diffX1)))
 
-    pySum[:, i] = 0.5 * (psiConj * ((psi_n1y - psi_n) / diffY1) + psiConj * ((psi_n2y - psi_n) / diffY2))
-    pySqrSum[:, i] = 0.5 * (psiConj * ((psi_n - 2 * psi_n1y + psi_n2y) / np.square(diffY1)))
+    pySum[:,i] = 0.5 * (psiConj*((psi_n1y-psi_n)/diffY1)+psiConj*((psi_n2y-psi_n)/diffY2))
+    pySqrSum[:,i] = (psiConj*((psi_n-2*psi_n1y + psi_n2y)/np.square(diffY1)))
 
-# print(pySum)
+#print(pySum)
 
-pxAvg = np.sum(pxSum, axis=1)
-pxSqrAvg = np.sum(pxSqrSum, axis=1)
+pxAvg = np.sum(pxSum,axis=1)
+pxSqrAvg = np.sum(pxSqrSum,axis=1)
 
-pyAvg = np.sum(pySum, axis=1)
-pySqrAvg = np.sum(pySqrSum, axis=1)
+pyAvg = np.sum(pySum,axis=1)
+pySqrAvg = np.sum(pySqrSum,axis=1)
 
-# Considering only the length
+#Considering only the length
 pxAvgL = np.abs(pxAvg)
 pxSqrAvgL = np.abs(pxSqrAvg)
 
@@ -111,11 +105,49 @@ pyAvgL = np.abs(pyAvg)
 pySqrAvgL = np.abs(pySqrAvg)
 
 # Find uncertainties
-deltaX = np.sqrt(xSqrAvg - np.square(xAvg))
-deltaY = np.sqrt(ySqrAvg - np.square(yAvg))
+deltaX = np.sqrt(xSqrAvg-np.square(xAvg))
+deltaY = np.sqrt(ySqrAvg-np.square(yAvg))
 
-deltaPX = np.sqrt(pxSqrAvgL - np.square(pxAvgL))
-deltaPY = np.sqrt(pySqrAvgL - np.square(pyAvgL))
+deltaPX = np.sqrt(pxSqrAvgL-np.square(pxAvgL))
+deltaPY = np.sqrt(pySqrAvgL-np.square(pyAvgL))
 
 f = plt.figure()
-f.plt
+plt.plot(times,xAvg)
+plt.plot(times,yAvg)
+plt.title('Average Position as Function of Time')
+plt.legend('x','y')
+plt.xlabel('Time (microseconds)')
+plt.ylabel('Position (cm)')
+plt.xlim([0, 1000])
+plt.ylim(([-6,6]))
+
+print(min(deltaX*deltaPX), min(deltaY*deltaPY))
+print(pxAvg)
+
+plt.plot(times,deltaX*deltaPX)
+
+plt.show()
+
+print('Is the momentum imaginary?: ',np.iscomplex(pxAvg))
+
+momentum_x = momentum_from_position(xAvg)
+momentum_y = momentum_from_position(yAvg)
+
+momentum_x_sqr = momentum_from_position(xSqrAvg)
+momentum_y_sqr = momentum_from_position(ySqrAvg)
+
+dpx = np.sqrt(momentum_x_sqr-(momentum_x*momentum_x))
+dpy = np.sqrt(momentum_y_sqr-(momentum_y*momentum_y))
+
+plt.figure()
+plt.plot(times,momentum_x)
+plt.plot(times,momentum_y)
+plt.title('Average Momentum as Function of Time')
+plt.legend('px','py')
+plt.xlabel('Time (microseconds)')
+plt.ylabel('Momentum (cm/mu s)')
+plt.xlim([0, 1000])
+plt.ylim(([-6,6]))
+plt.show()
+
+#animation_station(momentum_x,momentum_y,x,y,dpx,dpy)
--- projects/qm_brain/utils.py
+++ projects/qm_brain/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
-
+from matplotlib.animation import FuncAnimation
+import os
+from scipy.signal import hilbert
 
 def load_matrix(filepath):
     extension = filepath.split('.')[1]
@@ -14,28 +16,89 @@ def load_matrix(filepath):
     elif str(extension) == 'npz':
         return np.load(filepath)
 
+def process_eeg_csv(data,chanLocs):
+    x = chanLocs[:, 0]
+    y = chanLocs[:, 1]
 
-def animation_station(xAvg,yAvg,xInit,yInit):
-    fig = plt.figure(figsize=(10, 6))
-    ax = plt.axes(xlim=(-10, 10), ylim=(-8, 8))
-    line, = ax.plot([], [], lw=2)
+    hilbertTransData = hilbert(data, axis=1)
+
+    amplitude = np.abs(hilbertTransData).T
+
+    wavefunction = hilbertTransData
+
+    phase = np.unwrap(np.angle(hilbertTransData)).T
+
+    ampMag = np.sqrt(np.sum((amplitude * amplitude).T, axis=0))
+
+    normAmp = (np.asarray(amplitude.T) / np.asarray(ampMag)).T
+
+    probability = normAmp * normAmp
+
+    del ampMag, amplitude, hilbertTransData, chanLocs
+
+    return x, y, wavefunction, phase, normAmp, probability
+
+
+def animation_station2(xAvg,yAvg,xInit,yInit):
+    fig,ax = plt.subplots(figsize=(10, 6))
+    ax.set(xlim=(-10,10),ylim=(-8,8))
+
+    scat = ax.scatter(xAvg,yAvg)
 
     # initialization function: plot the background of each frame
     def init():
-        line.set_data(xInit, yInit)
-        return line,
+        ax.set_data(xInit, yInit)
+        return scat,
 
     # animation function.  This is called sequentially
-    def animate(i, xAvg, yAvg):
+    def animate(i):
         # fig.errorbar(xAvg[i], yAvg[i], dy, dy, dx, dx, marker='.')
-        x1 = xAvg  # [i]
-        y1 = yAvg  # [i]
-        line.set_data(x1, y1)
-        return line,
+        x1 = xAvg[i]
+        y1 = yAvg[i]
+        scat.set_data(x1, y1)
+        return scat,
 
     # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
+    anim = FuncAnimation(fig, animate, init_func=init,
                                    frames=300, interval=20)  # , blit=True)
 
     anim.save('animationTest.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
+def animation_station(xAvg,yAvg,xIn,yIn,xerr,yerr):
+    for i in range(300):
+        fig = plt.figure(figsize=(480/96, 480 / 96), dpi=96)
+        plt.scatter(xIn,yIn)
+        plt.scatter(xAvg[i], yAvg[i],edgecolors="grey", linewidth=2)
+        plt.xlim(-10, 10)
+        plt.ylim(-8, 8)
+        filename = 'step' + str(i) + '.png'
+        plt.savefig(filename, dpi=96)
+        plt.errorbar(xerr[i],yerr[i])
+        plt.gca()
+        plt.close(fig)
+    bashCommand = "convert -delay 80 *.png animated_chart_momentum_nick.gif"
+    os.system(bashCommand)
+
+def probability_conservation_plot(num_electrodes,data):
+    y = []
+    x=np.linspace(0,num_electrodes,num=num_electrodes)
+    for i in range(num_electrodes):
+        y.append(np.sum(data[i,:]))
+    plt.plot(x,y,color='r')
+    plt.show()
+
+def momentum_from_position(avg_position,ind=1):
+    momentum = []
+    for i in range(avg_position.shape[-1]):
+        if i == (avg_position.shape[-1]-1):
+            position = avg_position[i]
+            momentum.append(position/ind)
+        else:
+            position = avg_position[i + ind] - avg_position[i]
+            momentum.append(position / ind)
+    return np.asarray(momentum)
+
+def momentum_wavefunction(pos_wavefunc,norm='ortho',axis=1):
+    momentum_wavefunction = np.fft.fft(pos_wavefunc,norm=norm,axis=axis)
+    #p_y = np.fft.fft(y_wavefunc, norm=norm, axis=axis)
+    return momentum_wavefunction
